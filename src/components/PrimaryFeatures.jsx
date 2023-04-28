d688687892325e60baeafd771050071dc045a90f
mariananferreira <marianaferreira_17@hotmail.com>
2023-04-28 16:23:04 +0100

    second

--- src/components/PrimaryFeatures.jsx
+++ src/components/PrimaryFeatures.jsx
@@ -4,151 +4,143 @@ import { Tab } from '@headlessui/react'
 import clsx from 'clsx'
 
 import { Container } from '@/components/Container'
-import backgroundImage from '@/images/background-features.jpg'
-import screenshotExpenses from '@/images/screenshots/expenses.png'
-import screenshotPayroll from '@/images/screenshots/payroll.png'
-import screenshotReporting from '@/images/screenshots/reporting.png'
-import screenshotVatReturns from '@/images/screenshots/vat-returns.png'
+import mari from '@/images/mari1.jpg'
 
-const features = [
-  {
-    title: 'Payroll',
-    description:
-      "Keep track of everyone's salaries and whether or not they've been paid. Direct deposit not supported.",
-    image: screenshotPayroll,
-  },
-  {
-    title: 'Claim expenses',
-    description:
-      "All of your receipts organized into one place, as long as you don't mind typing in the data by hand.",
-    image: screenshotExpenses,
-  },
-  {
-    title: 'VAT handling',
-    description:
-      "We only sell our software to companies who don't deal with VAT at all, so technically we do all the VAT stuff they need.",
-    image: screenshotVatReturns,
-  },
-  {
-    title: 'Reporting',
-    description:
-      'Easily export your data into an Excel spreadsheet where you can do whatever the hell you want with it.',
-    image: screenshotReporting,
-  },
-]
 
 export function PrimaryFeatures() {
-  let [tabOrientation, setTabOrientation] = useState('horizontal')
-
-  useEffect(() => {
-    let lgMediaQuery = window.matchMedia('(min-width: 1024px)')
-
-    function onMediaQueryChange({ matches }) {
-      setTabOrientation(matches ? 'vertical' : 'horizontal')
-    }
-
-    onMediaQueryChange(lgMediaQuery)
-    lgMediaQuery.addEventListener('change', onMediaQueryChange)
-
-    return () => {
-      lgMediaQuery.removeEventListener('change', onMediaQueryChange)
-    }
-  }, [])
-
+  const [hideText, setHideText] = useState(false);
+  const toggleButton  = () => setHideText(true);
+  
   return (
     <section
       id="features"
       aria-label="Features for running your books"
-      className="relative overflow-hidden bg-blue-600 pb-28 pt-20 sm:py-32"
+      className="bg-slate-200 pb-28 pt-20 sm:py-32"
     >
-      <Image
-        className="absolute left-1/2 top-1/2 max-w-none translate-x-[-44%] translate-y-[-42%]"
-        src={backgroundImage}
-        alt=""
-        width={2245}
-        height={1636}
-        unoptimized
-      />
-      <Container className="relative">
-        <div className="max-w-2xl md:mx-auto md:text-center xl:max-w-none">
-          <h2 className="font-display text-3xl tracking-tight text-white sm:text-4xl md:text-5xl">
-            Everything you need to run your books.
-          </h2>
-          <p className="mt-6 text-lg tracking-tight text-blue-100">
-            Well everything you need if you aren’t that picky about minor
-            details like tax compliance.
-          </p>
-        </div>
-        <Tab.Group
-          as="div"
-          className="mt-16 grid grid-cols-1 items-center gap-y-2 pt-10 sm:gap-y-6 md:mt-20 lg:grid-cols-12 lg:pt-0"
-          vertical={tabOrientation === 'vertical'}
-        >
-          {({ selectedIndex }) => (
-            <>
-              <div className="-mx-4 flex overflow-x-auto pb-4 sm:mx-0 sm:overflow-visible sm:pb-0 lg:col-span-5">
-                <Tab.List className="relative z-10 flex gap-x-4 whitespace-nowrap px-4 sm:mx-auto sm:px-0 lg:mx-0 lg:block lg:gap-x-0 lg:gap-y-1 lg:whitespace-normal">
-                  {features.map((feature, featureIndex) => (
-                    <div
-                      key={feature.title}
-                      className={clsx(
-                        'group relative rounded-full px-4 py-1 lg:rounded-l-xl lg:rounded-r-none lg:p-6',
-                        selectedIndex === featureIndex
-                          ? 'bg-white lg:bg-white/10 lg:ring-1 lg:ring-inset lg:ring-white/10'
-                          : 'hover:bg-white/10 lg:hover:bg-white/5'
-                      )}
-                    >
-                      <h3>
-                        <Tab
-                          className={clsx(
-                            'font-display text-lg [&:not(:focus-visible)]:focus:outline-none',
-                            selectedIndex === featureIndex
-                              ? 'text-blue-600 lg:text-white'
-                              : 'text-blue-100 hover:text-white lg:text-white'
-                          )}
-                        >
-                          <span className="absolute inset-0 rounded-full lg:rounded-l-xl lg:rounded-r-none" />
-                          {feature.title}
-                        </Tab>
-                      </h3>
-                      <p
-                        className={clsx(
-                          'mt-2 hidden text-sm lg:block',
-                          selectedIndex === featureIndex
-                            ? 'text-white'
-                            : 'text-blue-100 group-hover:text-white'
-                        )}
-                      >
-                        {feature.description}
-                      </p>
-                    </div>
-                  ))}
-                </Tab.List>
+      <Container className="relative py-16">
+        <div
+          className="absolute inset-x-0 top-0 hidden h-1/2  lg:block"
+          aria-hidden="true"
+        />
+        <div className="mx-auto max-w-7xl lg:bg-transparent lg:px-8">
+          <div className="lg:grid lg:grid-cols-12">
+            <div className="relative z-10 lg:col-span-4 lg:col-start-1 lg:row-start-1 lg:bg-transparent lg:py-16">
+              <div
+                className="absolute inset-x-0 h-1/2 bg-gray-50 lg:hidden"
+               aria-hidden="true"
+              />
+              <div className="mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:max-w-none lg:p-0">
+                <div className="aspect-w-10 aspect-h-6 sm:aspect-w-2 sm:aspect-h-1 lg:aspect-w-1">
+                  <Image
+                   className="rounded-3xl object-cover object-center shadow-2xl"
+                   src={mari}
+                    alt=""
+                    priority
+                    />
+                </div>
               </div>
-              <Tab.Panels className="lg:col-span-7">
-                {features.map((feature) => (
-                  <Tab.Panel key={feature.title} unmount={false}>
-                    <div className="relative sm:px-6 lg:hidden">
-                      <div className="absolute -inset-x-4 bottom-[-4.25rem] top-[-6.5rem] bg-white/10 ring-1 ring-inset ring-white/10 sm:inset-x-0 sm:rounded-t-xl" />
-                      <p className="relative mx-auto max-w-2xl text-base text-white sm:text-center">
-                        {feature.description}
-                      </p>
-                    </div>
-                    <div className="mt-10 w-[45rem] overflow-hidden rounded-xl bg-slate-50 shadow-xl shadow-blue-900/20 sm:w-auto lg:mt-0 lg:w-[67.8125rem]">
-                      <Image
-                        className="w-full"
-                        src={feature.image}
-                        alt=""
-                        priority
-                        sizes="(min-width: 1024px) 67.8125rem, (min-width: 640px) 100vw, 45rem"
+            </div>
+            <div className="secondary relative lg:col-span-10 lg:col-start-3 lg:row-start-1 lg:grid lg:grid-cols-10 lg:items-center lg:rounded-3xl">
+              <div
+               className="absolute inset-0 hidden overflow-hidden rounded-3xl lg:block"
+               aria-hidden="true"
+              >
+                <svg
+                  className="absolute bottom-full left-full translate-y-1/3 -translate-x-2/3 transform xl:bottom-auto xl:top-0 xl:translate-y-0"
+                  width={404}
+                 height={384}
+                 fill="none"
+                 viewBox="0 0 404 384"
+                 aria-hidden="true"
+                >
+                 <defs>
+                    <pattern
+                     id="64e643ad-2176-4f86-b3d7-f2c5da3b6a6d"
+                     x={0}
+                      y={0}
+                      width={20}
+                      height={20}
+                     patternUnits="userSpaceOnUse"
+                    >
+                      <rect
+                         x={0}
+                          y={0}
+                          width={4}
+                          height={4}
+                          className="text-indigo-500"
+                         fill="currentColor"
                       />
-                    </div>
-                  </Tab.Panel>
-                ))}
-              </Tab.Panels>
-            </>
-          )}
-        </Tab.Group>
+                    </pattern>
+                  </defs>
+                  <rect
+                    width={404}
+                    height={384}
+                    fill="url(#64e643ad-2176-4f86-b3d7-f2c5da3b6a6d)"
+                  />
+                </svg>
+                <svg
+                  className="absolute top-full -translate-y-1/3 -translate-x-1/3 transform xl:-translate-y-1/2"
+                  width={404}
+                  height={384}
+                  fill="none"
+                  viewBox="0 0 404 384"
+                  aria-hidden="true"
+                >
+                  <defs>
+                    <pattern
+                     id="64e643ad-2176-4f86-b3d7-f2c5da3b6a6d"
+                     x={0}
+                     y={0}
+                     width={20}
+                     height={20}
+                      patternUnits="userSpaceOnUse"
+                    >
+                      <rect
+                        x={0}
+                        y={0}
+                        width={4}
+                        height={4}
+                          className=""
+                      fill="currentColor"
+                      />
+                    </pattern>
+                  </defs>
+                  <rect
+                    width={404}
+                    height={384}
+                   fill="url(#64e643ad-2176-4f86-b3d7-f2c5da3b6a6d)"
+                  />
+                </svg>
+              </div>
+              <div className="relative mx-auto max-w-md space-y-6 py-12 px-4 sm:max-w-3xl sm:py-16 sm:px-6 lg:col-span-6 lg:col-start-4 lg:max-w-none lg:p-0">
+                <h2 className="name">Mariana Guerreiro,</h2>
+                <h2
+                  className="text-2xl tracking-tight test"
+                  id="join-heading"
+                >
+                  NUTRICIONISTA <br/>
+                  NUTRICOACH                
+                </h2>
+                <p className="textAboutMe">
+                  Olá!<br/>
+                  Sou a Mariana Guerreiro, Nutricionista apaixonada pela área da alimentação e pela implementação de hábitos alimentares saudáveis.<br/>
+                  Pretendo ajudá-lo/a a concretizar os seus objetivos de forma eficaz.<br/>
+                  Consistência e dedicação são as chaves do sucesso!<br/>Bem-vindo/a ao meu website!</p>
+                <button 
+                  className="mt-2 inline-flex w-full items-center buttonStyle justify-center rounded-md border border-transparent bg-indigo-100 px-3 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-indigo-200 focus:ring-offset-2 sm:w-auto"
+                  >
+                    Saiba mais
+                </button>
+                <p className="textAboutMe">Segui por esta maravilhosa área porque adoro o ramo da saúde, onde posso ter impacto direto na vida das pessoas. A gratificação que se tem ao mudar a vida de alguém para melhor, não tem palavras.<br/>Licenciei-me na Faculdade de Ciências da Nutrição de Universidade do Porto e desde então tenho trabalho na área de Nutrição Clínica. <br/>
+                                                      Ao longo do meu percurso como estudante, e como pessoa ativa que sou, não me limitei apenas a estudar. Procurei realizar inúmeros estágios voluntários no sentido de me enriquecer como pessoa e futura profissional. Fazem parte destes: o acompanhamento de consultas de Nutrição em Clínicas privadas, o estágio Hospital da Santa Casa da Misericórdia de Marco de Canaveses e Lar da Santa Casa da Misericórdia de Marco de Canaveses; Centro de Medicina Desportiva do Porto entre outros projetos de educação alimentar em escolas básicas.<br/>
+                                                      Como a sabedoria é o bem mais precioso que temos, tirei um curso de Nutricoach para poder dar um serviço diferenciador aos meus pacientes.
+                </p> 
+              
+
+              </div>
+            </div>
+          </div>
+        </div>
       </Container>
     </section>
   )
