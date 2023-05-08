570eb2987c5ff858b512f76ce516669267a7c56c
mariananferreira <marianaferreira_17@hotmail.com>
2023-05-08 12:09:30 +0100

    mobile

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -206,20 +206,22 @@ export function Testimonials() {
                       alt=""
                       priority
                     />
-                    <p className="summmaryText marginAdjust">Ou ligue-me através do número:
+                    <p className="summmaryText marginAdjust">Ou via telefónica:
                     </p>              
 
                     <ul  className="mt-10 text-indigo-200 mobileIcon marginAdjust">
                       <li>
                           <span className="sr-only">Facebook
                           </span>
-                          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="20" fill="currentColor" class="bi bi-phone" viewBox="0 0 16 16">
-                            <path d="M11 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h6zM5 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H5z"/>
-                            <path d="M8 14a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"/>
-                          </svg>                        
+                          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-phone-call">
+                            <path d="M15.05 5A5 5 0 0 1 19 8.95M15.05 1A9 9 0 0 1 23 8.94m-1 7.98v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z">
+                            </path>
+                          </svg>
                       </li>
-                      <p className="mobileNumber">+351 938396971
-                      </p>
+                      <a href="tel:5551234567">
+                        <p className="mobileNumber">+351 938396971
+                        </p>
+                      </a>
                     </ul>
                   </div>
                 </div>
